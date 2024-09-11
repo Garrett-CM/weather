9dacdf84c93ab134fba56cb6e2f22db3079a7d8d
Garrett-CM <GarrettcalebMiller@gmail.com>
2024-09-10 19:12:05 -0500

    .env added for api calls

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
+
+  //header logic
   const [count, setCount] = useState(0)
 
   const handleAdd = () => {
@@ -14,6 +18,28 @@ function App() {
     setCount(count - 1)
   }
 
+  //weather logic
+  const apiKey = import.meta.env.VITE_WEATHER_APP_API_KEY; //look at me protecting the info
+
+  const [location, setLocation] = useState(' ')
+
+
+  //hmm cant get value at same time bc it udates the state at the end 
+  const updateLocation = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const getWeather = async () => {
+    console.log(`getting weather for ${location}`);
+    let weatherData = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`);
+    let weatherDataJSON = await weatherData.json();
+    console.log(weatherDataJSON)
+     //fetch weather data from API
+    //display weather data on the page
+  }
+
+
+
   return (
     <>
       <div>
@@ -21,6 +47,10 @@ function App() {
         <p>the count is: {count}</p> 
         <button onClick={handleAdd}> + </button> <button onClick={handleSubtract}> - </button>
       </div>     
+      <div>
+        <h2>where the weather is gonna be</h2>
+        <input type="text" placeholder='enter your city' onChange={updateLocation}/> <button onClick={getWeather}>enter</button>
+      </div>
     </>
   )
 }
